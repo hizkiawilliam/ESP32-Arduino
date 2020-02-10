02a4550c8a5de9ade23e6aa0277ecc73185813e2
Hizkia William Eben <37948131+hizkiawilliam@users.noreply.github.com>
2020-02-10 17:31:40 +0700

    Add files via upload

---
+++ WifiClient/WifiClient.ino
@@ -0,0 +1,21 @@
+#include "WiFi.h"
+ 
+const char* ssid = "Nokia 5.1 Plus";
+const char* password =  "damnnigga123";
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+ 
+}
+ 
+void loop() {}
